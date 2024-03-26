762a483db0c0e01eed0e4ac5185f01c04346bc96
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-03-26 18:22:15 +0100

    add PATCH and DELETE for contributor

--- SoftDesk_Support/api/views.py
+++ SoftDesk_Support/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from django.contrib.auth.models import Group
 
 from accounts.models import CustomUser
@@ -26,6 +27,40 @@ class GroupsViewSet(viewsets.ModelViewSet):
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        contributor_name = request.data.get('contributors')
+        if contributor_name is not None:
+            try:
+                user = CustomUser.objects.get(username=contributor_name)
+                Contributor.objects.get_or_create(user=user, project=instance)
+            except CustomUser.DoesNotExist:
+                return Response(
+                    {"error": f"l'utilisateur {contributor_name} n'existe pas"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        contributor_name = request.data.get('contributors')
+        if contributor_name is not None:
+            try:
+                user = CustomUser.objects.get(username=contributor_name)
+                contributor = Contributor.objects.get(
+                    user=user, project=instance)
+                contributor.delete()
+                serializer = self.get_serializer(instance)
+                return Response(serializer.data)
+            except (CustomUser.DoesNotExist, Contributor.DoesNotExist):
+                return Response(
+                    {"error": f"le contributeur {contributor_name} "
+                     + "n'existe pas dans ce projet"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        return super().destroy(request, *args, **kwargs)
+
 
 
 class ContributorViewSet(viewsets.ModelViewSet):
