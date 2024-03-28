213213e7860b8bbdfdabdb8a992d751d2b96437a
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-03-28 10:05:00 +0100

    add model to admin

--- SoftDesk_Support/api/admin.py
+++ SoftDesk_Support/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from project.models import Project, Contributor
+from issue.models import Issue, Comment
+
+admin.site.register(Project)
+admin.site.register(Contributor)
+admin.site.register(Issue)
+admin.site.register(Comment)
