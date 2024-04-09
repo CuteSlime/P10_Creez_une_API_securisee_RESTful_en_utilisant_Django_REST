fa0b7ffb4563320ac61c6471282ab85ee085882a
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-04-09 21:55:35 +0200

    add pagination

--- SoftDesk_Support/Config/settings.py
+++ SoftDesk_Support/Config/settings.py
@@ -88,7 +88,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
